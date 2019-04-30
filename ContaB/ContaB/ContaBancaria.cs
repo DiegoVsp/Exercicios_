5861ef09526592ebdc3407288919d941113968b7
Diego <diego-vespa2020@outlook.com.br>
2019-04-30 11:13:45 -0300

    Atualizando Exercicios

---
+++ ContaB/ContaB/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ContaB
+{
+    class ContaBancaria
+    {
+        private int Numero;
+        private string Titular;
+        private double Saldo;
+
+        public int Numero1 { get => Numero; private set => Numero = value; }
+        public string Titular1 { get => Titular; set => Titular = value; }
+        public double Saldo1 { get => Saldo; private set => Saldo = value; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+
+        }
+        public ContaBancaria(int numero, string titular, double depositoInicial):this(numero,titular)
+        {
+            Deposito(depositoInicial);
+        }
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+            Saldo -= 5.0; // Taxa de Saque
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " 
+                + Numero 
+                + ", Titular: " 
+                + Titular 
+                + ", Saldo: $" 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
